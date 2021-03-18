56dd636aa182fe35169da70021688ae1836ce996
Joao Santos <joao.santos@toptal.com>
2021-03-18 22:40:30 +0000

    Removed nodemon, removed test initial board.

--- server/controllers/game.js
+++ server/controllers/game.js
@@ -5,10 +5,8 @@ exports.gameStart = (req, res) => {
     const board = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
     const shuffled = shuffle(board);
 
-    const test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 0, 14, 15];
-
     return res.status(200).json({
-      data: test,
+      data: shuffled,
     });
   } catch (err) {
     return res.status(500).json({
